f8253f6286930bd48ce80672de4a4c2b386eabbe
User <User@LAB3-3>
2024-01-30 14:53:52 +0200

    saved

--- ViewModels/MonkeyPageViewModel.cs
+++ ViewModels/MonkeyPageViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿//using Android.Text.Method;
+using MonkeysMVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MonkeysMVVM.ViewModels
 {
-    internal class MonkeyPageViewModel:ViewModel
+    public class MonkeyPageViewModel:ViewModel
     {
+        private string country;
+        private int count;
+
+        public string Country {  get { return country; } set {  country = value; OnPropertyChanged();((Command)SearchByCountryCommand).ChangeCanExecute(); } }
+        public int Count { get { return count; } set { count = value; OnPropertyChanged(); } }
+      
+        public ICommand SearchByCountryCommand { get; set; }
+
+        private Monkey monkey;
+        public string name { get {  return monkey.Name; } }
+        public string ImageUrl {  get { return monkey.ImageUrl; } }
+
+        public MonkeyPageViewModel()
+        {
+            monkey = new Monkey() { Name = "אין קופים כרגע" };
+            SearchByCountryCommand = new Command(FindMonkeys,()=> Country != null);
+        }
+
+        private void FindMonkeys()
+        {
+           //not implemented 
+        }
     }
 }
